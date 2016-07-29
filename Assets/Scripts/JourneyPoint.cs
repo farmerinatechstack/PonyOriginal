ca9683b0523f4f96057595a6b15de9f309b6a6f8
Hassan Karaouni <hkII@DN525fhj.SUNet>
2016-07-29 12:34:23 -0700

    Add light trail and scripts

---
+++ Assets/Scripts/JourneyPoint.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Encapsulates the behavior for a point on a journey
+public class JourneyPoint : MonoBehaviour {
+	public bool containsContent;
+
+	protected bool enabled;
+
+	private Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		LightJourney parentJourney = transform.parent.GetComponent<LightJourney> ();
+		if (parentJourney == null) {
+			// TODO: throw exception
+		}
+
+		rend.material.color = Color.Lerp (parentJourney.offColor, parentJourney.endColor, parentJourney.lightBeat);
+	}
+}
---
+++ Assets/Scripts/LightJourney.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// Controls the light journey points from a travel start to destination
+public class LightJourney: JourneyPoint {
+	private List<JourneyPoint> points;		// The series of points on the journey
+	public float lightBeat;					// The 'heartbeat' of the journey lights, used for color and light
+
+	// Used to lerp the colors of the enabled journey lights
+	public Color offColor;					// Represent the off color of a journey light
+	public Color endColor;					// Represents the on color of a journey light
+
+	// Use this for initialization
+	void Start () {
+		points = new List<JourneyPoint> ();
+		foreach (JourneyPoint pt in transform.GetComponentsInChildren<JourneyPoint>()) {
+			points.Add (pt);
+		}
+		lightBeat = 0.0f;
+
+		// TODO: the following is hard coded for prototype purposes, will need event system
+		for (int i = 0; i < 6; i++) {
+			points [i].enabled = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float duration = 3f;
+		lightBeat = Mathf.PingPong (Time.time, duration) / duration; // Ping pong the value of light beat between 0 and 1
+	}
+}

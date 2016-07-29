d2a24587dd23a5fd67fd7eb1df335cfc77322bef
Hassan Karaouni <hkII@DN525fhj.SUNet>
2016-07-29 14:02:23 -0700

    Prototype light system

--- Assets/Scripts/JourneyPoint.cs
+++ Assets/Scripts/JourneyPoint.cs
@@ -4,23 +4,39 @@ using System.Collections;
 // Encapsulates the behavior for a point on a journey
 public class JourneyPoint : MonoBehaviour {
 	public bool containsContent;
+	public bool reached;
 
-	protected bool enabled;
-
+	private LightJourney parentJourney;
 	private Renderer rend;
+	private Light lightComponent;
 
 	// Use this for initialization
 	void Start () {
-		rend = GetComponent<Renderer> ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		LightJourney parentJourney = transform.parent.GetComponent<LightJourney> ();
+		parentJourney = transform.parent.GetComponent<LightJourney> ();
 		if (parentJourney == null) {
 			// TODO: throw exception
 		}
 
-		rend.material.color = Color.Lerp (parentJourney.offColor, parentJourney.endColor, parentJourney.lightBeat);
+		rend = GetComponent<Renderer> ();
+		rend.material.color = parentJourney.offColor;
+
+		if (containsContent) {
+			lightComponent = gameObject.transform.GetChild (0).GetComponent<Light> ();
+			if (lightComponent == null) {
+				// TODO: throw exception
+			}
+			lightComponent.range = 10;
+			lightComponent.color = parentJourney.onColor;
+			lightComponent.intensity = 0.0f;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (reached) {
+			rend.material.color = Color.Lerp (parentJourney.offColor, parentJourney.onColor, parentJourney.lightBeat);
+			if (containsContent)
+				lightComponent.intensity = parentJourney.lightBeat;
+		}
 	}
 }
--- Assets/Scripts/LightJourney.cs
+++ Assets/Scripts/LightJourney.cs
@@ -4,12 +4,12 @@ using System.Collections.Generic;
 
 // Controls the light journey points from a travel start to destination
 public class LightJourney: JourneyPoint {
-	private List<JourneyPoint> points;		// The series of points on the journey
-	public float lightBeat;					// The 'heartbeat' of the journey lights, used for color and light
-
+	public float lightBeat;				// The 'heartbeat' of the journey lights, used for color and light
 	// Used to lerp the colors of the enabled journey lights
 	public Color offColor;					// Represent the off color of a journey light
-	public Color endColor;					// Represents the on color of a journey light
+	public Color onColor;					// Represents the on color of a journey light
+
+	private List<JourneyPoint> points;		// The series of points on the journey
 
 	// Use this for initialization
 	void Start () {
@@ -21,13 +21,13 @@ public class LightJourney: JourneyPoint {
 
 		// TODO: the following is hard coded for prototype purposes, will need event system
 		for (int i = 0; i < 6; i++) {
-			points [i].enabled = true;
+			points [i].reached = true;
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float duration = 3f;
+		float duration = 2.0f;
 		lightBeat = Mathf.PingPong (Time.time, duration) / duration; // Ping pong the value of light beat between 0 and 1
 	}
 }
